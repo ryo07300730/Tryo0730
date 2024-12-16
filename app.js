622669d79d805215cd21adefd0e2f75c4bac46c4
ryo07300730 <23hs127@s.bgu.ac.jp>
2024-12-16 13:22:05 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,28 @@
-       function checkFortune() {
+     function checkFortune() {
+            var colour = document.getElementById('colour-select').value;
+            var number = parseInt(document.getElementById('number-input').value, 10);
+            var result = "凶"; // デフォルトの値
 
-    var weather = document.getElementById('weather-select').value; // 天気の取得
+            if (colour === "黒" && number === 0) {
+                result = "小吉";
+            } else if (colour === "黒" && number === 1) {
+                result = "中吉";
+            } else if (colour === "黒" && number === 2) {
+                result = "凶";
+            } else if (colour === "白" && number === 0) {
+                result = "中吉";
+            } else if (colour === "白" && number === 1) {
+                result = "中吉";
+            } else if (colour === "白" && number === 2) {
+                result = "大凶";
+            } else if (colour === "オレンジ" && number === 0) {
+                result = "凶";
+            } else if (colour === "オレンジ" && number === 1) {
+                result = "大吉";
+            } else if (colour === "オレンジ" && number === 2) {
+                result = "中吉";
+            }
 
-    var number = parseInt(document.getElementById('number-input').value, 10); // 数字の取得
-
-    var result = "普通"; // デフォルトの値
-
-    if (weather === "晴れ" && number === 0) {
-
-        result = "大吉";
-
-    } else if (weather === "晴れ" && number === 1) {
-
-        result = "中吉";
-
-    } else if (weather === "晴れ" && number === 2) {
-
-        result = "小吉";
-
-    } else if (weather === "雨" && number === 0) {
-
-        result = "中吉";
-
-    } else if (weather === "雨" && number === 1) {
-
-        result = "小吉";
-
-    } else if (weather === "雨" && number === 2) {
-
-        result = "凶";
-
-    } else if (weather === "曇り" && number === 0) {
-
-        result = "小吉";
-
-    } else if (weather === "曇り" && number === 1) {
-
-        result = "凶";
-
-    } else if (weather === "曇り" && number === 2) {
-
-        result = "大凶";
-
-    } else if (weather === "雪" && number === 0) {
-
-        result = "中吉";
-
-    } else if (weather === "雪" && number === 1) {
-
-        result = "大吉";
-
-    } else if (weather === "雪" && number === 2) {
-
-        result = "凶";
-
-    }
-
-    document.getElementById('result-output').innerText = "選んだ天気「" + weather + "」と数字「" + number + "」の結果は【" + result + "】です";
-
-} 
+            document.getElementById('result-output').innerText = "あなたの今週の運勢は【" + result + "】です";
+        }
+ 
